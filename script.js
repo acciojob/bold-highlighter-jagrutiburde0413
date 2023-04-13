890266192bca01238d60475587fed05b4f30f527
jagrutiburde0413 <124602129+jagrutiburde0413@users.noreply.github.com>
2023-04-14 00:29:15 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,12 +1,12 @@
-function highlight() {
-    //Write your code here
+  
+const para=document.getElementsByTagName("strong")
+const para1=document.getElementsByTagName("href")
 
-
-}
-
-
-function return_normal() {
-    //Write your code here
-
-    
-}
+ para1.addEventListener("mouseover",highlight)
+ function highlight(){
+    para.style.color="blue"
+ }
+ para1.addEventListener("mouseout",return_normal)
+function return_normal(){
+    para.style.color="black"
+ }
